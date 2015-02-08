2618da171ded35f87d6729817fbe2cebd18290a8
Linus Oleander <linus@oleander.nu>
2015-02-08 22:14:29 +0100

    Clean up specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,9 @@
 ENV["RAILS_ENV"] = "test"
 
-require File.expand_path("../../test/dummy/config/environment.rb",  __FILE__)
-ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../test/dummy/db/migrate", __FILE__)]
-require "rails/test_help"
-
-
+require_relative "dummy/config/environment"
 require "flyer"
-require "rails/version"
-#require "action_controller"
-#require "action_controller/test_case"
 require "timecop"
-require 'capybara/rails'
+require "capybara/rails"
 require "capybara/rspec"
 
 RSpec.configure do |config|
@@ -18,16 +11,4 @@ RSpec.configure do |config|
   config.order = :random
   config.include Capybara::DSL
   config.include Rails.application.routes.url_helpers
-end
-
-# Filter out Minitest backtrace while allowing backtrace from other libraries
-# to be shown.
-Minitest.backtrace_filter = Minitest::BacktraceFilter.new
-
-# Load support files
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
-
-# Load fixtures from the engine
-if ActiveSupport::TestCase.respond_to?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
 end
